03ad9bc7c77a651c1b926cbdff769a1112571556
aramzham <aramzham@hotmail.com>
2017-01-29 23:16:25 +0400

    up to Add

---
+++ DataStructures/DynamicArrays/MyList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DynamicArrays
+{
+    class MyList<T> : IEnumerable
+    {
+        #region Constructors
+
+        public MyList() : this(0)
+        {
+
+        }
+
+        public MyList(int length)
+        {
+            if (length < 0) throw new ArgumentOutOfRangeException();
+
+            array = new T[length];
+        }
+
+        public MyList(ICollection<T> collection)
+        {
+            var index = 0;
+            var newArray = new T[collection.Count];
+            foreach (var item in collection)
+            {
+                Count++;
+                newArray[index++] = item;
+            }
+            array = newArray;
+        }
+
+        #endregion
+
+        private T[] array;
+        public int Count { get; set; }
+        public int Capacity => array.Length;
+
+        public int IndexOf(T item)
+        {
+            for (var i = 0; i < array.Length; i++)
+                if (array[i].Equals(item)) return i;
+
+            return -1;
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) != -1;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException();
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= Count) throw new ArgumentOutOfRangeException();
+                array[index] = value;
+            }
+        }   // indexer
+
+        public void Add(T item)
+        {
+            if (Count == array.Length) GrowArray();
+
+            array[Count++] = item;
+        }
+
+        private void GrowArray()
+        {
+            var newSize = array.Length == 0 ? 4 : array.Length * 2;
+            var newArray = new T[newSize];
+            Array.Copy(array, 0, newArray, 0, array.Length);
+            array = newArray;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        } // a thing that i still don't understand :-)
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+    }
+}
---
+++ DataStructures/DynamicArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace DynamicArrays
+{
+    class Program
+    {
+
+        public static void Print(IEnumerable obj)
+        {
+            foreach (var item in obj)
+                Console.Write($"{item} ");
+        }
+        static void Main(string[] args)
+        {
+            var words = new[] { "I", "see", "no", "changes.", "Woke", "up", "in", "the", "morning", "and", "asked", "myself" };
+            var myArray = new MyList<string>(words);
+            Print(myArray);
+            Console.WriteLine("\n" + myArray.IndexOf("now"));
+            Console.WriteLine(myArray.Contains("the"));
+            Console.WriteLine(words[5]);
+            words[11] = "yourself";
+            //Console.WriteLine(words[12]); words[-1] = "something";  // this two cases bring exceptions
+            Print(myArray);
+
+            var anotherArray = new MyList<int>();
+            anotherArray.Add(1);
+            anotherArray.Add(1);
+            anotherArray.Add(1);
+            anotherArray.Add(1);
+            anotherArray.Add(1);
+            Console.WriteLine(anotherArray.Capacity);
+
+            Console.ReadKey();
+        }
+    }
+}

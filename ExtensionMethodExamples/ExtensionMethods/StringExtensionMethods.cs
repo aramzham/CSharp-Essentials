3a7563508bba2a446ca72e8bc3623353ff620c23
aramzham <aramzham@hotmail.com>
2017-01-30 00:32:34 +0400

    how do you like extension methods? :)
    Signed-off-by: aramzham <aramzham@hotmail.com>

---
+++ ExtensionMethodExamples/ExtensionMethods/StringExtensionMethods.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class StringExtensionMethods
+    {
+        public static bool IsPalindrome(this string str)
+        {
+            str = str.ToLower();
+            return !str.Where((t, i) => t != str[str.Length - 1 - i]).Any();
+        }
+
+        public static string ReplaceWithSymbols(this string str)
+        {
+            str = str.Replace(":)", "\u2323");
+            str = str.Replace(":(", "\u2639");
+            str = str.Replace("->", "\u2192");
+            str = str.Replace("<-", "\u2190");
+            return str;
+        }
+
+        public static string ToArmenian(this string text)
+        {
+            var map = new Dictionary<string, string>
+            {
+            // specific armenian letters
+                {"@", "ը"},
+                {"th", "թ"},
+                {"zh", "ժ"},
+                {"&", "ծ"},
+                {"dz", "ձ"},
+                {"gh", "ղ"},
+                {"sh", "շ"},
+                {"ch", "չ"},     // +ճ
+                {"ph", "փ"},
+                {"ev", "և"},
+
+            //standard latin letters
+                {"a", "ա"},
+                {"b", "բ"},
+                {"c", "ց"},
+                {"d", "դ"},
+                {"e", "ե"},    // +է
+                {"f", "ֆ"},
+                {"g", "գ"},
+                {"h", "հ"},
+                {"i", "ի"},
+                {"j", "ջ"},
+                {"k", "կ"},
+                {"l", "լ"},
+                {"m", "մ"},
+                {"n", "ն"},
+                {"o", "ո"},
+                {"p", "պ"},
+                {"q", "ք"},
+                {"r", "ր"},    // +ռ
+                {"s", "ս"},
+                {"t", "տ"},
+                {"u", "ու"},
+                {"v", "վ"},
+                {"w", "ինչ ես գրում w-ով՞"},
+                {"x", "խ"},
+                {"y", "յ"},
+                {"z", "զ"},
+
+            };
+
+            return map.Aggregate(text, (current, pair) => current.Replace(pair.Key, pair.Value));
+        }
+    }
+}
---
+++ ExtensionMethodExamples/TestArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ExtensionMethods;
+
+namespace TestArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.OutputEncoding = System.Text.Encoding.UTF8;
+            var s = "Արամ";
+            Console.WriteLine(s.IsPalindrome());
+            Console.WriteLine("\u0531\u0580\u0561\u0574");
+            Console.WriteLine("\u2323");
+
+            var str = "I'll be staying there -> with my friends :) do you want to come with us?<- or not?:()";
+            Console.WriteLine(str.ReplaceWithSymbols());
+
+            var translitText = "chanaprah@ shat erkar tvac indz bayc cicernakner@ prkecin gonya";
+            Console.WriteLine(translitText.ToArmenian());
+
+            Console.ReadKey();
+        }
+    }
+}

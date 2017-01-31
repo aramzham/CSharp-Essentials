df6e4609eabf4c721cb341e3e100e742978297d6
aramzham <aramzham@hotmail.com>
2017-02-01 00:28:33 +0400

    files created

--- IDisposable/EmailGrabber/Program.cs
+++ IDisposable/EmailGrabber/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using SpamSender;
 
 namespace EmailGrabber
 {
@@ -10,6 +8,18 @@ namespace EmailGrabber
     {
         static void Main(string[] args)
         {
+            var outputFile = @"outputtext.txt";
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            var HTMLFile = Path.Combine(desktop, outputFile);
+            const string site = "http://www.mail.ru/";
+            using (var downloader = new HTMLDownloader(HTMLFile))
+            {
+                downloader.Save(site, HTMLFile); 
+            }
+
+            var mailFinder = new FindWriteEmails();
+            var emailsFile = @"C:\Users\HP\Desktop\emails.txt";
+            mailFinder.WriteEmailsInFile(HTMLFile, emailsFile);
         }
     }
 }

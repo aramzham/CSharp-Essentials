aa3fe3b06ab82e9565363786a8253ff93310ecc7
aramzham <aramzham@hotmail.com>
2017-02-24 18:57:08 +0400

    main

--- DataStructures/Hash table/Program.cs
+++ DataStructures/Hash table/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hash_table
 {
@@ -10,6 +6,18 @@ namespace Hash_table
     {
         static void Main(string[] args)
         {
+            var instance = new HashTable<int, string>(5);
+
+            instance.Add(0, "Hello");
+            instance.Add(1, "World");
+            instance.Add(2, "Again");
+            instance.Add(3, "!");
+            instance.Add(4, "To increase the array lenght");
+
+            foreach (var item in instance)
+            {
+                Console.WriteLine(item.Value);
+            }
         }
     }
 }

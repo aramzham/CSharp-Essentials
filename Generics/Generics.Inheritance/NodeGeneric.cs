fa923c6dceb1d819145c18422a4f1c0d62a3d7cc
Aram Zhamkochyan <aramzham@hotmail.com>
2020-03-07 18:02:22 +0400

    generics initial, generic node type added

---
+++ Generics/Generics.Inheritance/NodeGeneric.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Inheritance
+{
+    internal sealed class NodeGeneric<T>
+    {
+        public T m_data;
+        public NodeGeneric<T> m_next;
+
+        public NodeGeneric(T data) : this(data, null)
+        {
+
+        }
+
+        public NodeGeneric(T data, NodeGeneric<T> next)
+        {
+            this.m_data = data;
+            this.m_next = next;
+        }
+
+        public override String ToString()
+        {
+            return m_data.ToString() + ((m_next != null) ? m_next.ToString() : string.Empty);
+        }
+    }
+}
---
+++ Generics/Generics.Inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SameDataLinkedList();
+
+            Console.ReadKey();
+        }
+
+        static void SameDataLinkedList() 
+        {
+            var head = new NodeGeneric<char>('C');
+            head = new NodeGeneric<char>('B', head);
+            head = new NodeGeneric<char>('A', head);
+            Console.WriteLine(head.ToString());
+        }
+    }
+}

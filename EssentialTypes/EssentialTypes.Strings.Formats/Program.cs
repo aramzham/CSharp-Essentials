13faa3dee19ab4d4b8bae4a976c6dd04426f6d38
Aram Zhamkochyan <aramzham@hotmail.com>
2020-03-15 18:19:21 +0400

    string format

---
+++ EssentialTypes/EssentialTypes.Strings.Formats/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTypes.Strings.Formats
+{
+    class MyClass : IFormattable
+    {
+        private int _value;
+
+        public MyClass(int value)
+        {
+            _value = value;
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return string.Format("{0}, now = {1}", _value.ToString(format, formatProvider), DateTime.Now.ToString("U", formatProvider));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new MyClass(10);
+            Console.WriteLine(m.ToString());
+            Console.WriteLine(m.ToString("C", new CultureInfo("ru-RU")));
+            Console.WriteLine(m.ToString("F", CultureInfo.InvariantCulture));
+        }
+    }
+}
--- EssentialTypes/EssentialTypes.Strings.StringBuilders/Program.cs
+++ EssentialTypes/EssentialTypes.Strings.StringBuilders/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace EssentialTypes.Strings.StringBuilders
     {
         static void Main(string[] args)
         {
-            var sb = new StringBuilder(1,2);
+            var sb = new StringBuilder(1, 2);
             Console.WriteLine($"Max capacity = {sb.MaxCapacity}");
             Console.WriteLine($"Capacity = {sb.Capacity}");
 

9cbce17ccee1eb25ce5ff76fb87e93f036be1be2
Aram Zhamkochyan <aramzham@hotmail.com>
2020-05-09 16:57:40 +0400

    quickstart and deepClone by serialization

---
+++ CoreFacilities/CoreFacilities.Serialization.DeepClone/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreFacilities.Serialization.DeepClone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new object();
+            var second = DeepClone(first);
+            Console.WriteLine($"Are objects the same? - {ReferenceEquals(first, second)}");
+
+            Console.ReadKey();
+        }
+
+        private static object DeepClone(object original)
+        {
+            // Construct a temporary memory stream
+            using (var stream = new MemoryStream())
+            {
+                // Construct a serialization formatter that does all the hard work
+                var formatter = new BinaryFormatter
+                {
+                    Context = new StreamingContext(StreamingContextStates.Clone)
+                };
+                // Serialize the object graph into the memory stream
+                formatter.Serialize(stream, original);
+                // Seek back to the start of the memory stream before deserializing
+                stream.Position = 0;
+                // Deserialize the graph into a new set of objects and
+                // return the root of the graph (deep copy) to the caller
+                return formatter.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ CoreFacilities/CoreFacilities.Serialization.QuickStart/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreFacilities.Serialization.QuickStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a graph of objects to serialize them to the stream
+            var objectGraph = new List<string> { "Levon", "Robert", "Serj", "Nikol" };
+            Stream stream = SerializeToMemory(objectGraph);
+            // Reset everything for this demo
+            stream.Position = 0;
+            objectGraph = null;
+            // Deserialize the objects and prove it worked
+            objectGraph = (List<string>)DeserializeFromMemory(stream);
+            foreach (var s in objectGraph) 
+                Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+
+        private static MemoryStream SerializeToMemory(object objectGraph)
+        {
+            // Construct a stream that is to hold the serialized objects
+            var stream = new MemoryStream();
+            // Construct a serialization formatter that does all the hard work
+            var formatter = new BinaryFormatter();
+            // Tell the formatter to serialize the objects into the stream
+            formatter.Serialize(stream, objectGraph);
+            // Return the stream of serialized objects back to the caller
+            return stream;
+        }
+        private static object DeserializeFromMemory(Stream stream)
+        {
+            // Construct a serialization formatter that does all the hard work
+            var formatter = new BinaryFormatter();
+            // Tell the formatter to deserialize the objects from the stream
+            return formatter.Deserialize(stream);
+        }
+    }
+}

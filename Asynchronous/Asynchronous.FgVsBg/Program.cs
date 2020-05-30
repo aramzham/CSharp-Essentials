ab1d7589dfa541f690ebfe8bc881032efa40432a
Aram Zhamkochyan <aramzham@hotmail.com>
2020-05-30 17:31:32 +0400

    added project showing difference between foreground and background threads

---
+++ Asynchronous/Asynchronous.FgVsBg/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Asynchronous.FgVsBg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new thread (defaults to foreground)
+            var t = new Thread(Worker);
+// Make the thread a background thread
+            t.IsBackground = true;
+            t.Start(); // Start the thread
+// If t is a foreground thread, the application won't die for about 10 seconds
+// If t is a background thread, the application dies immediately
+            Console.WriteLine("Returning from Main");
+        }
+        
+        private static void Worker() {
+            Thread.Sleep(10000); // Simulate doing 10 seconds of work
+// The following line only gets displayed if this code is executed by a foreground thread
+            Console.WriteLine("Returning from Worker");
+        }
+    }
+}
---
+++ CoreFacilities/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static Regex imageUrlRegex = new Regex("/upload.*?\\.jpg", RegexOptions.Compiled);
+
+        static void Main(string[] args)
+        {
+            var links = File.ReadAllLines(@"C:\Users\Aram\Desktop\atkritkas\links.txt");
+            var count = 0;
+            foreach (var link in links)
+            {
+                try
+                {
+                    Console.WriteLine(count);
+                    var page = SendGetRequest(link);
+                    var url = GetUrl(page);
+                    File.AppendAllText(@"C:\Users\Aram\Desktop\atkritkas\imageLinks.txt", $"http://www.atkritka.com{url}{Environment.NewLine}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    count++;
+                }
+            }
+        }
+
+        private static void DownloadFile(string url, string fileName)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(url, fileName);
+            }
+        }
+
+        private static string SendGetRequest(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                return webClient.DownloadString(url);
+            }
+        }
+
+        private static string GetUrl(string page)
+        {
+            var url = imageUrlRegex.Match(page).Groups[0].Value;
+            return url;
+        }
+    }
+}

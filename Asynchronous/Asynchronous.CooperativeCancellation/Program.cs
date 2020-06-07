b78f1d1c5ca331ca4ef7a92ab6c737a697b47906
Aram Zhamkochyan <aramzham@hotmail.com>
2020-06-07 20:19:31 +0400

    cooperative cancellation

---
+++ Asynchronous/Asynchronous.CooperativeCancellation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Asynchronous.CooperativeCancellation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+// Pass the CancellationToken and the number­to­count­to into the operation
+            ThreadPool.QueueUserWorkItem(o => Count(cts.Token, 100));
+            Console.WriteLine("Press <Enter> to cancel the operation.");
+            Console.ReadLine();
+            cts.Cancel(); // If Count returned already, Cancel has no effect on it
+// Cancel returns immediately, and the method continues running here...
+            Console.ReadLine();
+        }
+
+        private static void Count(in CancellationToken token, int countTo)
+        {
+            for (Int32 count = 0; count <countTo; count++) {
+                if (token.IsCancellationRequested) {
+                    Console.WriteLine("Count is cancelled");
+                    break; // Exit the loop to stop the operation
+                }
+                Console.WriteLine(count);
+                Thread.Sleep(200); // For demo, waste some time
+            }
+            Console.WriteLine("Count is done");
+        }
+    }
+}
---
+++ Asynchronous/Asynchronous.LinkedTokenSource/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+
+namespace Asynchronous.LinkedTokenSource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            // it seems like cts has a queue of actions that it cancels
+            cts.Token.Register(() => Console.WriteLine("Canceled 1"));
+            cts.Token.Register(() => Console.WriteLine("Canceled 2"));
+            cts.Token.Register(() => Console.WriteLine("Canceled 3"));
+            
+            cts.Cancel();
+
+            Console.ReadKey();
+            
+            // Create a CancellationTokenSource
+            var cts1 = new CancellationTokenSource();
+            cts1.Token.Register(() => Console.WriteLine("cts1 canceled"));
+            
+            // Create another CancellationTokenSource
+            var cts2 = new CancellationTokenSource();
+            cts2.Token.Register(() => Console.WriteLine("cts2 canceled"));
+            
+            // Create a new CancellationTokenSource that is canceled when cts1 or ct2 is canceled
+            var linkedCts = CancellationTokenSource.CreateLinkedTokenSource(cts1.Token, cts2.Token);
+            linkedCts.Token.Register(() => Console.WriteLine("linkedCts canceled...means that one of tokens is cancelled or linkedCts himself is cancelled"));
+
+            // Cancel one of the CancellationTokenSource objects (I chose cts2)
+            cts2.Cancel();
+            
+            // Display which CancellationTokenSource objects are canceled
+            Console.WriteLine("cts1 canceled={0}, cts2 canceled={1}, linkedCts canceled={2}",
+                cts1.IsCancellationRequested, cts2.IsCancellationRequested,
+                linkedCts.IsCancellationRequested);
+            
+            var cts3 = new CancellationTokenSource(/*TimeSpan.FromSeconds(2)*/);
+            cts3.CancelAfter(2000);
+            cts3.Token.Register(()=>Console.WriteLine("cts3 is cancelled after 2 seconds"));
+            cts.Cancel();
+            
+            Console.ReadKey();
+        }
+    }
+}

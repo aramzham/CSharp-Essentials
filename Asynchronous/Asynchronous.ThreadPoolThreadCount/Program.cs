fca2eae13d8c82c951f1d3b64b66fa0bd15da25a
Aram Zhamkochyan <aramzham@hotmail.com>
2020-06-28 23:18:17 +0400

    playing with thread pool set, get thread pool thread counts

---
+++ Asynchronous/Asynchronous.ThreadPoolThreadCount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchronous.ThreadPoolThreadCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GetMaxThreads , SetMaxThreads , GetMinТhreads , SetMinThreads , and GetAvailableThreads
+            ThreadPool.SetMinThreads(0, 0); // min is (4, 4) for my machine
+            ThreadPool.SetMaxThreads(13, 5);
+            ThreadPool.GetMaxThreads(out var workerThreads, out var completionPortThreads);
+            Console.WriteLine($"Max thread count = {workerThreads}, completion port threads = {completionPortThreads}");
+            ThreadPool.GetMinThreads(out workerThreads, out completionPortThreads); // min threads count = processors count
+            Console.WriteLine($"Min thread count = {workerThreads}, completion port threads = {completionPortThreads}");
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Run(Work);
+            }
+
+            ThreadPool.GetAvailableThreads(out workerThreads, out completionPortThreads);
+            Console.WriteLine($"There are {workerThreads} worker threads");
+            Console.WriteLine($"There are {completionPortThreads} completion port threads");
+
+            Console.ReadKey();
+        }
+
+        static void Work()
+        {
+            Console.WriteLine($"Working in thread {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000);
+        }
+    }
+}

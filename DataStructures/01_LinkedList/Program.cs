5ac76b0203fc3d2d76c228ee917adb6cb037bfe8
Aram Zhamkochyan <aramzham@hotmail.com>
2021-01-24 23:52:26 +0400

    collections initial commit

---
+++ DataStructures/01_LinkedList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootNode = new Node() {Value = 10};
+            rootNode.Add(20).Add(30).Add(40);
+            Console.WriteLine(rootNode);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Node
+    {
+        public int Value { get; set; }
+        private Node _next;
+
+        public Node Add(int value)
+        {
+            _next = new Node() { Value = value };
+            return _next;
+        }
+
+        public override string ToString() => $"{Value}, next: {_next?.Value}";
+    }
+}
---
+++ DataStructures/02_Enumerable/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Reflection.Metadata;
+
+namespace _02_Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootNode = new ListNode() { Value = 10 };
+            rootNode.Add(20).Add(30);
+            var enumerator = rootNode.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current;
+                Console.WriteLine(item);
+            }
+            enumerator.Reset();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class ListNode : IEnumerable
+    {
+        public int Value { get; set; }
+        private ListNode _next;
+        private ListNode _last;
+
+        public ListNode Add(int value)
+        {
+            _next = new ListNode() { Value = value };
+            return _next;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}, next: {_next?.Value}";
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        private class Enumerator : IEnumerator
+        {
+            private ListNode _node;
+
+            public object? Current { get; private set; }
+
+            public Enumerator(ListNode node)
+            {
+                _node = node;
+            }
+
+            public bool MoveNext()
+            {
+                if (_node is null)
+                    return false;
+
+                Current = _node;
+                _node = _node._next;
+                return true;
+            }
+
+            public void Reset()
+            {
+                _node = null;
+            }
+        }
+    }
+}

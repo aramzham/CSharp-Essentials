09a137253049a3a6272c4947b929f8760e93e3b5
Aram Zhamkochyan <aramzham@hotmail.com>
2021-01-29 00:52:24 +0400

    some examples of own linq implementation

---
+++ LINQ/01_Linq/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Linq
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> MyFind<T>(this IEnumerable<T> source, Func<T, bool> predicateFunc)
+        {
+            foreach (var item in source)
+            {
+                if(predicateFunc.Invoke(item))
+                    yield return item;
+            }
+        }
+
+        public static IEnumerable<T> MyTake<T>(this IEnumerable<T> source, int count)
+        {
+            if (count <= 0) 
+                yield break;
+
+            foreach (var item in source)
+            {
+                if (count == 0)
+                    yield break;
+                else
+                    yield return item;
+                
+                count--;
+            }
+        }
+
+        public static IEnumerable<IGrouping<TKey, TValue>> MyGroupBy<TKey, TValue>(this IEnumerable<TValue> source,
+            Func<TValue, TKey> keySelector)
+        {   
+            var dict = new Dictionary<TKey, Grouping<TKey, TValue>>();
+            foreach (var item in source)
+            {
+                var key = keySelector.Invoke(item);
+                if (!dict.TryGetValue(key, out var group))
+                {
+                    group = new Grouping<TKey, TValue>();
+                    dict.Add(key, group);
+                }
+
+                group.Add(item);
+            }
+
+            return dict.Values;
+        }
+
+        private class Grouping<TKey, TValue> : List<TValue>, IGrouping<TKey, TValue>
+        {
+            public TKey Key { get; }
+        }
+    }
+}
---
+++ LINQ/01_Linq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randoms = RandomGenerator(); // 100 ints in range of 0 to 1000
+            var odds = randoms.MyFind(x => x % 2 == 1);
+            var two = randoms.MyTake(2).ToList();
+            var groups = randoms.MyGroupBy(x => x % 3);
+            var linqGroups = randoms.GroupBy(x => x % 3);
+            var linqToLookup = randoms.ToLookup(x => x % 3);
+
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> RandomGenerator()
+        {
+            var r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                yield return r.Next(0, 1001);
+            }
+        }
+    }
+}

dc8700ed27f6b37ccc45efe594c61d530261eda7
Aram Zhamkochyan <aramzham@hotmail.com>
2021-02-17 20:49:21 +0400

    examples on threads

---
+++ Asynchronous/Threads_01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Test
+{
+    public class Program
+    {
+        private static ConcurrentDictionary<int, char> _dictionary = new ConcurrentDictionary<int, char>();
+        //private static Dictionary<int, char> _dictionary = new Dictionary<int, char>();
+
+        static void Main()
+        {
+            var reader = new Thread(Read);
+            var writer = new Thread(Writer);
+            var remover = new Thread(Remove);
+
+            reader.Start();
+            writer.Start();
+            remover.Start();
+
+            Console.ReadKey();
+        }
+
+        static void Read()
+        {
+            while (true)
+            {
+                Thread.Sleep(10);
+                var key = 10;
+
+                if (_dictionary.ContainsKey(key))
+                    Console.Write(_dictionary[key]);
+
+                //_dictionary.TryGetValue(key, out var value);
+                //Console.Write(value);
+            }
+        }
+
+        static void Writer()
+        {
+            while (true)
+            {
+                Thread.Sleep(10);
+
+                var key = 10;
+                _dictionary[key] = '.';
+            }
+        }
+
+        static void Remove()
+        {
+            while (true)
+            {
+                Thread.Sleep(10);
+
+                var key = 10;
+                //_dictionary.Remove(key);
+                _dictionary.TryRemove(key, out _);
+            }
+        }
+    }
+}
---
+++ Asynchronous/Threads_02_Optimization/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads_02_Optimization
+{
+    class Program
+    {
+        private static bool stopWorker = false;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main: letting worker run for 1 seconds");
+
+            var t = new Thread(Worker);
+            t.Start();
+            Thread.Sleep(500);
+            stopWorker = true;
+            Console.WriteLine("Main: waiting for worker to stop");
+            Console.ReadLine();
+        }
+
+        private static void Worker(object state)
+        {
+            var x = 0;
+
+            while (!stopWorker)
+            {
+                x++;
+            }
+
+            Console.WriteLine($"Worker: stopped at: {x}");
+        }
+
+        private static void OptimizedAway()
+        {
+            int value = (1 * 100) - (50 * 2);
+
+            for (int i = 0; i < value; i++)
+            {
+                Console.WriteLine("Opmtimized away");
+            }
+        }
+    }
+}

336afc065fbb2fff4181ed458f79bed7f77ada9a
Aram Zhamkochyan <aramzham@hotmail.com>
2021-02-28 00:04:45 +0400

    expressions_copy

---
+++ LINQ/07_Expressions_Copy/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+using _Common_Expressions;
+
+namespace _07_Expressions_Copy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Student() { Age = 19, Name = "Vahram" };
+            Expression<Func<Student, Student>> copyExpression = student => new Student() { Age = student.Age, Name = student.Name };
+            Func<Student, Student> copyFunc = copyExpression.Compile();
+
+            var clone = copyFunc(st);
+            Console.WriteLine($"{clone.Age}, {clone.Name}");
+
+            Console.ReadKey();
+        }
+    }
+}
